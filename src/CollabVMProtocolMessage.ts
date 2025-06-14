b65f9ad909a7f3146dd9ca428afde53e4c606ee1
wize-logic <wizelogic@proton.me>
2025-06-13 22:24:52 -0400

    Update CollabVMProtocolMessage.ts

--- src/CollabVMProtocolMessage.ts
+++ src/CollabVMProtocolMessage.ts
@@ -3,9 +3,11 @@ import { CollabVMRectMessage } from "./CollabVMRectMessage.js";
 export interface CollabVMProtocolMessage {
     type: CollabVMProtocolMessageType;
     rect?: CollabVMRectMessage | undefined;
+    opusPacket?: Buffer | Uint8Array | undefined; // Opus audio packet
 }
 
 export enum CollabVMProtocolMessageType {
     // JPEG Dirty Rectangle
     rect = 0,
+    audioOpus = 1 // Opus audio message type
 }
